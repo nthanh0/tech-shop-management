4f229f4963374aeb114e89a50c59a9cebd119399
Phan Cao Huy <huyphan0594@gmail.com>
2026-03-26 18:14:27 +0700

    Huy-CreatAdminArea-AdminLayout

---
+++ BTL_API/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_API.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BTL_API/Program.cs
+++ BTL_API/Program.cs
@@ -21,6 +21,10 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    name: "MyArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
